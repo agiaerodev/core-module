5a091a388ed8a106cf8f2a30636f0210861f4194
Jose Vivas <jvivas@imaginacolombia.com>
2026-01-09 20:48:49 -0400

    +Update: V2 stage for message handling back and forth

---
+++ SyncHandlers/UniversalDispatcher.cs
@@ -0,0 +1,41 @@
+﻿using Core.SyncHandlers.Interfaces;
+using Ihelpers.Messages.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.SyncHandlers.Messages
+{
+
+    public class UniversalDispatcher : IMessageHandler<JObject>
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public UniversalDispatcher(IServiceProvider serviceProvider) => _serviceProvider = serviceProvider;
+
+        public async Task HandleAsync(JObject message)
+        {
+            string entityStr = message["entity_target"]?.ToString() ?? message["target_entity"]?.ToString();
+            if (string.IsNullOrEmpty(entityStr)) return;
+
+            Type entityType = Type.GetType(entityStr);
+            if (entityType == null) return;
+
+            
+            Type handlerType = typeof(IEntitySyncHandler<>).MakeGenericType(entityType);
+
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var handler = scope.ServiceProvider.GetService(handlerType);
+                if (handler != null)
+                {
+                    var method = handlerType.GetMethod("HandleAsync");
+                    await (Task)method.Invoke(handler, new object[] { message });
+                }
+            }
+        }
+    }
+}
